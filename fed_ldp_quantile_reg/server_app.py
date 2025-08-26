f65250a7ea09399a73ec25eb525107776914b746
chensiting <chensiting@163.sufe.edu.cn>
2025-08-27 01:06:33 +0800

    use Polyak-Ruppert averaging

--- fed_ldp_quantile_reg/server_app.py
+++ fed_ldp_quantile_reg/server_app.py
@@ -1,11 +1,44 @@
 """test: A Flower / PyTorch app."""
 
-from flwr.common import Context, ndarrays_to_parameters
+from flwr.common import Context, ndarrays_to_parameters,parameters_to_ndarrays
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
 from fed_ldp_quantile_reg.quantile_task import QuantileNet, get_weights
 import numpy as np
 from scipy.stats import norm
+
+
+class FedPolyakRuppert(FedAvg):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.history_sum = None
+        self.round_count = 0
+
+    def aggregate_fit(self, rnd, results, failures):
+        # perform FedAvg aggregation to get the round's averaged parameters.
+        aggregated_params, _ = super().aggregate_fit(rnd, results, failures)
+        if aggregated_params is None:
+            return None, {}
+
+        # turn parameters into numpy
+        aggregated_ndarrays = parameters_to_ndarrays(aggregated_params)
+        # print('#'*100)
+        # print('aggregated_ndarrays',aggregated_ndarrays)
+
+        # Polyak-Ruppert averaging
+        if self.history_sum is None:
+            self.history_sum = [np.array(p, copy=True) for p in aggregated_ndarrays]
+        else:
+            # print('self.history_sum',self.history_sum) 
+            for i in range(len(self.history_sum)):
+                self.history_sum[i] += aggregated_ndarrays[i]
+            # print('self.history_sum',self.history_sum) 
+
+        self.round_count += 1
+        averaged_params = [p / self.round_count for p in self.history_sum]
+        # print('averaged_params',averaged_params)
+        return ndarrays_to_parameters(averaged_params), {}
+
 
 def get_evaluate_fn(tau):
     """Evaluate model parameters on the server."""
@@ -18,15 +51,9 @@ def get_evaluate_fn(tau):
         bias = parameters[1]
         
         beta_pred = np.concatenate([bias, weights.flatten()])
+        mse = np.mean((beta_pred - beta_true) ** 2)        
         
-        mse = np.mean((beta_pred - beta_true) ** 2)
-        
-        print(f"\n--- 第{server_round}轮模型参数 ---")
-        print(f"真实参数: {beta_true}")
-        print(f"估计参数: {beta_pred}")
-        print(f"整体 MSE: {mse:.6f}")
-        
-        return float(mse), {"mse": float(mse)}
+        return float(mse), {"round": server_round, "mse": float(mse), "beta_pred": [round(x, 6) for x in beta_pred.tolist()]}
     
     return evaluate
 
@@ -41,7 +68,7 @@ def server_fn(context: Context):
     parameters = ndarrays_to_parameters(ndarrays)
 
     # Define strategy
-    strategy = FedAvg(
+    strategy = FedPolyakRuppert(
         fraction_fit=fraction_fit,
         fraction_evaluate=1.0,
         min_available_clients=2,
